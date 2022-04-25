2ad360cb188e3d89c0d5d8f2eb4274d79ac4e770
ilytvynenko <lytvynenko@stud.onu.edu.ua>
2022-04-25 11:39:24 +0300

    Initial commit

---
+++ ReadingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReadingApp
+//
+//  Created by Inna Litvinenko on 25.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
